d3d8262e145426232a26951f8d351f161f2eb786
Andrew <aandrewjuan1@gmail.com>
2025-05-15 22:28:22 +0800

    feat: added custom colors and font

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="bg-sage font-family-primary">
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
 </template>
